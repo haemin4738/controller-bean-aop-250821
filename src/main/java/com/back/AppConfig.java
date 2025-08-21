99ca1f1d0566d615c3171b36203e33941297ba18
haemin4738 <leeheamin12@gmail.com>
2025-08-21 12:35:31 +0900

    AppConfig @Bean -> Service @Service

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,12 +1,7 @@
 package com.back;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    PersonService personService() {
-        return new PersonService();
-    }
 }
