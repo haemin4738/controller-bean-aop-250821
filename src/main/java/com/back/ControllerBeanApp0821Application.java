d28de800ff07a06ed7a1279621475e076344aaac
haemin4738 <leeheamin12@gmail.com>
2025-08-21 12:10:16 +0900

    Controller

--- src/main/java/com/back/ControllerBeanApp0821Application.java
+++ src/main/java/com/back/ControllerBeanApp0821Application.java
@@ -1,4 +1,4 @@
-package com.controllerbeanapp0821;
+package com.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/back/HomeController.java
@@ -0,0 +1,24 @@
+package com.back;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    private final PersonService personService = new PersonService();
+
+    @GetMapping("/")
+    @ResponseBody
+    public String main() {
+        long count = personService.count();
+        return "main // 사람 수 : %d".formatted(count);
+    }
+
+    @GetMapping("/people")
+    @ResponseBody
+    public String people() {
+        long count = personService.count();
+        return "people // 사람 수 : %d".formatted(count);
+    }
+}
