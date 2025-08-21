5a502fc504c52bd99f6ef6f9e4f97038ad2c0cfd
haemin4738 <leeheamin12@gmail.com>
2025-08-21 14:44:41 +0900

    AppConfig

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,5 +1,7 @@
 package com.back;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,7 +13,33 @@ public class AppConfig {
     }
 
     @Bean
-    PersonRepository personRepositoryV2() {
-        return new PersonRepository(2);
+    int version() {
+        return 3;
+    }
+
+    @Bean
+    public ApplicationRunner myApplicationRunner() {
+        return new ApplicationRunner() {
+            @Override
+            public void run (ApplicationArguments args) throws Exception {
+                System.out.println("myApplicationRunner is running");
+            }
+        };
+    }
+
+    @Bean
+    public ApplicationRunner myApplicationRunner2() {
+        return args -> {
+            work1();
+            work2();
+        };
+    }
+
+    private void work1() {
+        System.out.println("work1");
+    }
+
+    private void work2() {
+        System.out.println("work2");
     }
 }
